2e21399db3af8c6bc756f0b77476b9510d82fb96
BalajiKesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-05-08 00:01:51 -0700

    chore: Update Snowflake connection form layout and improve user experience

--- widgets/uploader.py
+++ widgets/uploader.py
@@ -3,8 +3,8 @@ import streamlit as st
 from utils.data_analysis_helpers import gather_metadata
 
 def render_uploader():
-
-        st.write(':blue[Upload your csv files to get started!]')
+    with st.form(key='upload_files'):
+        st.write(':blue[or upload your csv files to get started!]')
         st.session_state['uploaded_files'] = st.file_uploader(
             label='Upload Your Files in .csv format:', 
             type=['csv'],
@@ -12,7 +12,7 @@ def render_uploader():
             label_visibility='collapsed'
         )
 
-        st.button(
+        st.form_submit_button(
             label=':green[Lets Go!]',
             on_click=gather_metadata,
             use_container_width=True,
