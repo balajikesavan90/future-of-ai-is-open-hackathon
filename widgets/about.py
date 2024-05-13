10957a87f6ab0d6c743ce6208a080128cb12c49f
BalajiKesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-05-12 19:16:41 -0700

    feat: Update About page content and links

--- widgets/about.py
+++ widgets/about.py
@@ -10,10 +10,13 @@ def render_about():
         with text_col:
             st.write(
                 """
-                My name is Balaji Kesavan and I am a data scientist and I have a passion for AI. I am always looking for ways to improve my skills and learn new things.
-                This is my submission for the Future of AI is open hackathon. I hope you like it!
+                My name is [Balaji Kesavan](https://www.linkedin.com/in/balaji-kesavan/) and I am passionate about open source AI. I am always looking for ways to improve my skills and learn new things.
+                This is my submission to [The future of AI is open](https://arctic-streamlit-hackathon.devpost.com/) hackathon. I hope you like it!
+
+                You can access the source code for this project on [GitHub](https://github.com/balajikesavan90/future-of-ai-is-open-hackathon)
                 """
             )
+            
             with st.expander(':blue[What is Arctic Analytics?]', expanded=True):
                 st.write(
                     """
@@ -27,10 +30,11 @@ def render_about():
                 st.write(
                     """
                     The AI model does not have access to your data. It does however have access to the metadata of your data. In particular, it has access to the 
-                    - primary_keys
+                    - primary keys
                     - column names
                     - data types
-                    - data description
+                    - dataset description
+                    - column descriptions
                     - output of the pandas describe method on your data
                     - first 5 rows of your data. 
                     """
@@ -38,7 +42,7 @@ def render_about():
             with st.expander(':blue[What can the AI actually see?]', expanded=True):
                 st.write(
                     """
-                    After you load your add and starting engaiging with the AI, you can view the prompt that the AI sees on your sidebar.
+                    After you load your data and starting engaging with the AI, you can view the prompt that the AI sees on your sidebar.
                     """
                 )
     with photo_col:
