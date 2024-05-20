9f7d5c023551ef0ec5e6263dd6b408821b702b66
BalajiKesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-05-19 18:21:54 -0700

    added youtube video to about page

--- widgets/about.py
+++ widgets/about.py
@@ -7,6 +7,8 @@ def render_about():
     with about_me_container:
         text_col, photo_col = st.columns([3, 1])
 
+
+        st.video('https://www.youtube.com/watch?v=X7Vq1BdRgrk')
         with text_col:
             st.write(
                 """
@@ -16,8 +18,8 @@ def render_about():
                 You can access the source code for this project on [GitHub](https://github.com/balajikesavan90/future-of-ai-is-open-hackathon)
                 """
             )
-            
-            with st.expander(':blue[What is Arctic Analytics?]', expanded=True):
+
+            with st.expander(':blue[What is Arctic Analytics?]', expanded=False):
                 st.write(
                     """
                     Arctic Analytics is an AI tool that can 
@@ -26,7 +28,7 @@ def render_about():
                     - help document and debug your codebase.
                         """
                 )
-            with st.expander(':blue[Does the AI have access to my data?]', expanded=True):
+            with st.expander(':blue[Does the AI have access to my data?]', expanded=False):
                 st.write(
                     """
                     The AI model does not have access to your data. It does however have access to the metadata of your data. In particular, it has access to the 
@@ -39,12 +41,12 @@ def render_about():
                     - first 5 rows of your data. 
                     """
                 )
-            with st.expander(':blue[What can the AI actually see?]', expanded=True):
+            with st.expander(':blue[What can the AI actually see?]', expanded=False):
                 st.write(
                     """
                     After you load your data and starting engaging with the AI, you can view the prompt that the AI sees on your sidebar.
                     """
                 )
     with photo_col:
-        st.image('balaji.jpg', width=200, caption='Balaji Kesavan')
+        st.image('balaji.jpg', width=300, caption='Balaji Kesavan')            
                 
