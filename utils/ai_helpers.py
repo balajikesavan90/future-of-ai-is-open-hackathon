401771d54ddf9ae5c4247bbe30e3112f6cc991d7
Balaji Kesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-07-27 15:42:35 -0700

    Session isolation (#66)
    * killed chart builder
    
    * updated requirements
    
    * killing chart builder wip
    
    * Refactor generate_gpt4o_mini_response function to remove unused 'page' parameter
    
    * Refactor render_data_analyst function to use st.caption instead of st.subheader and st.success/st.info for improved styling
    
    * Refactor about page to include correct links and contact information
    
    * chore: Refactor open_ai_helpers.py to improve token counting and error handling
    
    * chore: Remove chart builder functionality
    
    * updated requirements
    
    * consolidated-data-analyst-and-chart-builder
    
    * added session_id
    
    * added logging
    
    * created metallama class
    
    * klled document and debug
    
    ---------
    
    Signed-off-by: Balaji Kesavan <36139209+balajikesavan90@users.noreply.github.com>

--- utils/ai_helpers.py
+++ utils/ai_helpers.py
@@ -3,13 +3,13 @@ import os
 import json
 import logging
 
-from utils.snowflake_arctic_helpers import construct_arctic_prompt, generate_arctic_response
-from utils.meta_llama_helpers import construct_llama_prompt, generate_llama_response
-from utils.mistral_helpers import construct_mistral_prompt, generate_mistral_response
+# from utils.snowflake_arctic_helpers import construct_arctic_prompt, generate_arctic_response
+from utils.meta_llama_helpers import MetaLlama
+# from utils.mistral_helpers import construct_mistral_prompt, generate_mistral_response
 from utils.open_ai_helpers import generate_gpt4o_mini_response
 from utils.system_messages import generate_explanation_system_message, generate_docstring_system_message, generate_debugger_system_message
 
-os.environ['REPLICATE_API_TOKEN'] = st.secrets['REPLICATE_API_TOKEN']
+llama_client = MetaLlama()
 
 def construct_welcome_message():
     logging.info(f'construct_welcome_message - {st.session_state["session_id"]}')
@@ -28,61 +28,22 @@ I have access to the metadata of the files you uploaded. I will use that to gene
         for file_name in st.session_state['vetted_files']:
             column_names = ', '.join(st.session_state["vetted_files"][file_name]["columns_names"])
             welcome_message += f'\n- The pandas dataframe :blue[{file_name}] has the columns: :blue[{column_names}].'
-
-
     return welcome_message
-
 
 def generate_ai_response(vetted_files, model):
     logging.info(f'generate_ai_response - {st.session_state["session_id"]}')
-    if model == 'arctic':
-        prompt_str = construct_arctic_prompt(vetted_files)
-        response = generate_arctic_response(prompt_str)
-    elif model == 'llama-3.1':
-        prompt_str = construct_llama_prompt(vetted_files)
-        response = generate_llama_response(prompt_str)
-    elif model == 'mistral':
-        prompt_str = construct_mistral_prompt(vetted_files)
-        response = generate_mistral_response(prompt_str)
+    if model == 'llama-3.1':
+        prompt_str = llama_client.construct_llama_prompt(vetted_files)
+        response = llama_client.generate_llama_response(prompt_str)
     elif model == 'gpt-4o-mini':
         response = generate_gpt4o_mini_response(vetted_files)
+    # elif model == 'mistral':
+    #     prompt_str = construct_mistral_prompt(vetted_files)
+    #     response = generate_mistral_response(prompt_str)
+    # elif model == 'arctic':
+    #     prompt_str = construct_arctic_prompt(vetted_files)
+    #     response = generate_arctic_response(prompt_str)
     return response
-    
-def generate_explanation_response(code_snippet):
-    logging.info(f'generate_explanation_response - {st.session_state["session_id"]}')
-    with st.session_state['output_container']:
-        with st.spinner('Generating Explanation...'):
-            prompt = [f"<|im_start|>system\n{generate_explanation_system_message}<|im_end|>"]
-            prompt.append('<|im_start|>user\n' + code_snippet + '<|im_end|>')
-            prompt.append('<|im_start|>assistant')
-            prompt.append('')
-            prompt_str = '\n'.join(prompt)
-            return generate_arctic_response(prompt_str)
-    
-def generate_docstring_response(code_snippet):
-    logging.info(f'generate_docstring_response - {st.session_state["session_id"]}')
-    with st.session_state['output_container']:
-        with st.spinner('Generating Docstrings...'):
-            prompt = [f"<|im_start|>system\n{generate_docstring_system_message}<|im_end|>"]
-            prompt.append('<|im_start|>user\n' + code_snippet + '<|im_end|>')
-            prompt.append('<|im_start|>assistant')
-            prompt.append('')
-            prompt_str = '\n'.join(prompt)
-            return generate_arctic_response(prompt_str)
-
-def generate_debugger_response(code_snippet, error_message):
-    logging.info(f'generate_debugger_response - {st.session_state["session_id"]}')
-    with st.session_state['output_container']:
-        with st.spinner('Generating Debugger Response...'):
-            prompt = [f"<|im_start|>system\n{generate_debugger_system_message}<|im_end|>"]
-            input = {'code_snippet': code_snippet, 'error_message': error_message}
-            input_json = json.dumps(input)
-            prompt.append('<|im_start|>user\n' + input_json + '<|im_end|>')
-            prompt.append('<|im_start|>assistant')
-            prompt.append('')
-            prompt_str = '\n'.join(prompt)
-            return generate_arctic_response(prompt_str)
-
 
 
 
--- utils/snowflake_arctic_helpers.py
+++ utils/snowflake_arctic_helpers.py
@@ -1,77 +1,77 @@
-import streamlit as st
-import replicate
-from transformers import AutoTokenizer
-import json
-import logging
+# import streamlit as st
+# import replicate
+# from transformers import AutoTokenizer
+# import json
+# import logging
 
-temperature = 0.1
-top_p = 0.1
+# temperature = 0.1
+# top_p = 0.1
 
-from utils.system_messages import construct_system_message
-from utils.streamlit_helpers import reset_data_analyst
+# from utils.system_messages import construct_system_message
+# from utils.streamlit_helpers import reset_data_analyst
 
 
-def construct_arctic_prompt(page, vetted_files):
-    logging.info(f'construct_arctic_prompt - {st.session_state["session_id"]}')
+# def construct_arctic_prompt(page, vetted_files):
+#     logging.info(f'construct_arctic_prompt - {st.session_state["session_id"]}')
 
-    prompt = [f"<|im_start|>system\n{construct_system_message(page, vetted_files)}<|im_end|>"]
-    for dict_message in st.session_state['messages']:
-        if dict_message['role'] == 'user':
-            user_input = json.dumps({'user_input': dict_message['content']})
-            prompt.append('<|im_start|>user\n' + user_input + '<|im_end|>\n')
-        else:
-            prompt.append('<|im_start|>assistant\n' + dict_message['content'] + '<|im_end|>\n')
+#     prompt = [f"<|im_start|>system\n{construct_system_message(page, vetted_files)}<|im_end|>"]
+#     for dict_message in st.session_state['messages']:
+#         if dict_message['role'] == 'user':
+#             user_input = json.dumps({'user_input': dict_message['content']})
+#             prompt.append('<|im_start|>user\n' + user_input + '<|im_end|>\n')
+#         else:
+#             prompt.append('<|im_start|>assistant\n' + dict_message['content'] + '<|im_end|>\n')
     
-    prompt.append('<|im_start|>assistant\n')
-    prompt.append('')
-    return '\n'.join(prompt)
+#     prompt.append('<|im_start|>assistant\n')
+#     prompt.append('')
+#     return '\n'.join(prompt)
 
-def generate_arctic_response(prompt_str):
-        logging.info(f'generate_arctic_response - {st.session_state["session_id"]}')
-        token_count = get_num_tokens(prompt_str)
-        logging.info(f'token_count - {token_count} - {st.session_state["session_id"]}')
+# def generate_arctic_response(prompt_str):
+#         logging.info(f'generate_arctic_response - {st.session_state["session_id"]}')
+#         token_count = get_num_tokens(prompt_str)
+#         logging.info(f'token_count - {token_count} - {st.session_state["session_id"]}')
 
-        error_count = 0
-        for message in st.session_state['messages']:
-            if 'error' in message.keys():
-                if message['role'] == 'assistant':
-                    error_count += 1
+#         error_count = 0
+#         for message in st.session_state['messages']:
+#             if 'error' in message.keys():
+#                 if message['role'] == 'assistant':
+#                     error_count += 1
         
-        if error_count >= 3:
-            st.error('Oops! Something went wrong. Try rephrasing your prompt in a different way.')
-            st.button(':red[Reset Data Analyst]', on_click=reset_data_analyst, key='reset')
-            if st.secrets['ENV'] == 'dev':
-                st.write(st.session_state['messages'])
-            st.stop()
+#         if error_count >= 3:
+#             st.error('Oops! Something went wrong. Try rephrasing your prompt in a different way.')
+#             st.button(':red[Reset Data Analyst]', on_click=reset_data_analyst, key='reset')
+#             if st.secrets['ENV'] == 'dev':
+#                 st.write(st.session_state['messages'])
+#             st.stop()
 
-        if token_count >= 3072:
-            st.error('Conversation length too long. Please keep it under 3072 tokens.')
-            st.button(':red[Reset Data Analyst]', on_click=reset_data_analyst, key='reset')
-            if st.secrets['ENV'] == 'dev':
-                st.write(st.session_state['messages'])
-            st.stop()
+#         if token_count >= 3072:
+#             st.error('Conversation length too long. Please keep it under 3072 tokens.')
+#             st.button(':red[Reset Data Analyst]', on_click=reset_data_analyst, key='reset')
+#             if st.secrets['ENV'] == 'dev':
+#                 st.write(st.session_state['messages'])
+#             st.stop()
         
-        events = []
-        st.session_state['prompt_str'] = prompt_str
-        for event in replicate.stream('snowflake/snowflake-arctic-instruct',
-                            input={'prompt': prompt_str,
-                                    'prompt_template': r"{prompt}",
-                                    'temperature': 0,
-                                    'top_p': top_p,
-                                    }):
-            events.append(str(event))
-        return ''.join(events)
+#         events = []
+#         st.session_state['prompt_str'] = prompt_str
+#         for event in replicate.stream('snowflake/snowflake-arctic-instruct',
+#                             input={'prompt': prompt_str,
+#                                     'prompt_template': r"{prompt}",
+#                                     'temperature': 0,
+#                                     'top_p': top_p,
+#                                     }):
+#             events.append(str(event))
+#         return ''.join(events)
 
-def get_num_tokens(prompt):
-    """Get the number of tokens in a given prompt"""
-    tokenizer = get_tokenizer()
-    tokens = tokenizer.tokenize(prompt)
-    return len(tokens)
+# def get_num_tokens(prompt):
+#     """Get the number of tokens in a given prompt"""
+#     tokenizer = get_tokenizer()
+#     tokens = tokenizer.tokenize(prompt)
+#     return len(tokens)
 
-@st.cache_resource(show_spinner=False)
-def get_tokenizer():
-    logging.info(f'get_tokenizer - {st.session_state["session_id"]}')
-    """Get a tokenizer to make sure we're not sending too much text
-    text to the Model. Eventually we will replace this with ArcticTokenizer
-    """
-    return AutoTokenizer.from_pretrained('huggyllama/llama-7b')
+# @st.cache_resource(show_spinner=False)
+# def get_tokenizer():
+#     logging.info(f'get_tokenizer - {st.session_state["session_id"]}')
+#     """Get a tokenizer to make sure we're not sending too much text
+#     text to the Model. Eventually we will replace this with ArcticTokenizer
+#     """
+#     return AutoTokenizer.from_pretrained('huggyllama/llama-7b')
