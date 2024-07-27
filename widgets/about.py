00c2e2d42b800befb348ebbcadb05fa2c8f5872e
Balaji Kesavan <36139209+balajikesavan90@users.noreply.github.com>
2024-07-26 22:41:21 -0700

    55 consolidate data analyst and chart builder (#59)
    * killed chart builder
    
    * updated requirements
    
    * killing chart builder wip
    
    * Refactor generate_gpt4o_mini_response function to remove unused 'page' parameter
    
    * Refactor render_data_analyst function to use st.caption instead of st.subheader and st.success/st.info for improved styling
    
    * Refactor about page to include correct links and contact information
    
    ---------
    
    Signed-off-by: Balaji Kesavan <36139209+balajikesavan90@users.noreply.github.com>

--- widgets/about.py
+++ widgets/about.py
@@ -5,49 +5,42 @@ def render_about():
     about_me_container = st.container()
 
     with about_me_container:
-        text_col, photo_col = st.columns([3, 1])
+        st.write(
+            """
+            My name is [Balaji Kesavan](https://www.balajikesavan.com/) and I am passionate about AI. I am always looking for ways to improve my skills and learn new things.
+            This is my submission to [The future of AI is open](https://arctic-streamlit-hackathon.devpost.com/) hackathon. I hope you like it!
 
+            You can access the source code for this project on [GitHub](https://github.com/balajikesavan90/future-of-ai-is-open-hackathon).
+            The best way to reach me is on [LinkedIn](https://www.linkedin.com/in/balaji-kesavan/).
+            """
+        )
 
-        st.video('https://www.youtube.com/watch?v=X7Vq1BdRgrk')
-        with text_col:
+        with st.expander(':blue[What is Arctic Analytics?]', expanded=False):
             st.write(
                 """
-                My name is [Balaji Kesavan](https://www.balajikesavan.com/) and I am passionate about AI. I am always looking for ways to improve my skills and learn new things.
-                This is my submission to [The future of AI is open](https://arctic-streamlit-hackathon.devpost.com/) hackathon. I hope you like it!
-
-                You can access the source code for this project on [GitHub](https://github.com/balajikesavan90/future-of-ai-is-open-hackathon).
-                The best way to reach me is on [LinkedIn](https://www.linkedin.com/in/balaji-kesavan/).
+                Arctic Analytics is an AI tool that can 
+                - answer questions about your data.
+                - build charts and graphs from your data.
+                - help document and debug your codebase.
+                    """
+            )
+        with st.expander(':blue[Does the AI have access to my data?]', expanded=False):
+            st.write(
+                """
+                The AI model does not have access to your data. It does however have access to the metadata of your data. In particular, it has access to the 
+                - primary keys
+                - column names
+                - data types
+                - dataset description
+                - column descriptions
+                - output of the pandas describe method on your data
+                - first 5 rows of your data. 
                 """
             )
-
-            with st.expander(':blue[What is Arctic Analytics?]', expanded=False):
-                st.write(
-                    """
-                    Arctic Analytics is an AI tool that can 
-                    - answer questions about your data.
-                    - build charts and graphs from your data.
-                    - help document and debug your codebase.
-                        """
-                )
-            with st.expander(':blue[Does the AI have access to my data?]', expanded=False):
-                st.write(
-                    """
-                    The AI model does not have access to your data. It does however have access to the metadata of your data. In particular, it has access to the 
-                    - primary keys
-                    - column names
-                    - data types
-                    - dataset description
-                    - column descriptions
-                    - output of the pandas describe method on your data
-                    - first 5 rows of your data. 
-                    """
-                )
-            with st.expander(':blue[What can the AI actually see?]', expanded=False):
-                st.write(
-                    """
-                    After you load your data and starting engaging with the AI, you can view the prompt that the AI sees on your sidebar.
-                    """
-                )
-    with photo_col:
-        st.image('balaji.jpg', width=300, caption='Balaji Kesavan')            
+        with st.expander(':blue[What can the AI actually see?]', expanded=False):
+            st.write(
+                """
+                After you load your data and starting engaging with the AI, you can view the prompt that the AI sees on your sidebar.
+                """
+            )
                 
